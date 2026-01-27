a82ddf10a668ed1b44065db902f6697f378dbbbd
Stefan B. <developer-stefan@outlook.com>
2026-01-27 07:13:20 +0200

    Updated LiveExample with selection dropdown and multiple example codes.

--- src/components/LiveExample/index.tsx
+++ src/components/LiveExample/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import * as React from "react";
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 
 import * as Core from "web-music-score-v6/core";
@@ -10,21 +10,37 @@ import * as Theory from "web-music-score-v6/theory";
 import * as Pieces from "web-music-score-v6/pieces";
 import * as ReactUI from "web-music-score-v6/react-ui";
 import BrowserOnly from "@docusaurus/BrowserOnly";
-import * as React from "react";
 
-export function LiveExample(props: { code: string, onEdit?: (newCode: string) => void }) {
-    const [code, setCode] = useState(props.code);
+type CodeEntry = { name: string, code: string };
+
+function getCodeEntries(codes: (string | CodeEntry) | (string | CodeEntry)[]): CodeEntry[] {
+    return (Array.isArray(codes) ? codes : [codes]).map((entry, entryId, entryArr) => {
+        if (typeof entry === "string") {
+            const name = entryArr.length > 1 ? ("Example " + (entryId + 1)) : "";
+            const code = name === "" ? entry.trim() : `// ${name}\n${entry.trim()}`;
+            return { name, code };
+        }
+        else {
+            const name = entry.name.trim();
+            const code = name === "" ? entry.code.trim() : `// ${name}\n${entry.code.trim()}`;
+            return { name, code };
+        }
+    });
+}
+
+function SingleLiveExample(props: { entry: CodeEntry, onEdit?: (newCode: string) => void }) {
+    const [entryCode, setEntryCode] = React.useState(props.entry.code);
 
     const onEdit = (newCode: string) => {
         if (props.onEdit) props.onEdit(newCode);
-        setCode(newCode);
+        setEntryCode(newCode);
         Audio.stop();
     };
 
     return <BrowserOnly>
         {() => {
             return <>
-                <LiveProvider language="jsx" code={code.trim()} scope={{ Core, Audio, AudioCG, AudioSynth, Score, Theory, Pieces, ReactUI, React }} >
+                <LiveProvider language="jsx" code={entryCode} scope={{ Core, Audio, AudioCG, AudioSynth, Score, Theory, Pieces, ReactUI, React }} >
                     <LiveEditor onChange={onEdit} />
                     <LiveError />
                     <br />
@@ -35,4 +51,34 @@ export function LiveExample(props: { code: string, onEdit?: (newCode: string) =>
             </>;
         }}
     </BrowserOnly>;
+}
+
+export function LiveExample(props: { code: (string | CodeEntry) | (string | CodeEntry)[] }) {
+    const [exampleId, setExampleId] = React.useState(0);
+    const entries = getCodeEntries(props.code);
+    const entry = entries[exampleId];
+
+    return <>
+        {entries.length > 1
+            ? <>
+                <select
+                    value={exampleId}
+                    onChange={e => setExampleId(Number(e.target.value))}
+                    className="select"
+                >
+                    {entries.map((entry, id) => (
+                        <option key={id} value={id}>
+                            {entry.name}
+                        </option>
+                    ))}
+                </select>
+                <br /><br />
+            </>
+            : null
+        }
+        <SingleLiveExample
+            key={exampleId}
+            entry={entry}
+        />
+    </>;
 }
