47c84486f39a1eda709c46a3116709d72fbf897e
Stefan B. <developer-stefan@outlook.com>
2026-05-23 19:32:16 +0300

    Show all blog entries in sidebar.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -47,6 +47,8 @@ const config: Config = {
         },
         blog: {
           showReadingTime: true,
+          blogSidebarCount: 'ALL',
+          blogSidebarTitle: 'All posts',
           feedOptions: {
             type: ['rss', 'atom'],
             xslt: true,
