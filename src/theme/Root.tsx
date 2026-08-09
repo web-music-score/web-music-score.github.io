38084f816e5e305e6ea1e5ea7f2952bedbd5833a
Stefan <web-music-score@outlook.com>
2026-08-09 13:57:16 +0300

    Removed Clarity analytics.

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,12 +1,13 @@
-import { useEffect } from "react";
-import { initClarity } from "../analytics/clarity";
+// import { useEffect } from "react";
+// import { initClarity } from "../analytics/clarity";
 
-const WMS_CLARITY_ID = "v1aw2dgkm7";
+// const WMS_CLARITY_ID = "v1aw2dgkm7";
 
 export default function Root({ children }) {
+  /*
     useEffect(() => {
         initClarity(WMS_CLARITY_ID);
     }, []);
-
+  */
   return <>{children}</>;
 }
