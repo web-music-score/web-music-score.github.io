10722dc7428e67581df62fec3dcc0729d20e1389
Stefan <web-music-score@outlook.com>
2026-09-21 21:45:35 +0300

    Updated Set Colors -example for v7.

--- src/docs/examples/v7/set-colors.tsx
+++ src/docs/examples/v7/set-colors.tsx
@@ -3,9 +3,9 @@ function ExampleCode() {
     const doc = new Score.DocumentBuilder({ color: "red", background: "lightyellow" })
         .setScoreConfiguration("guitarCombined")
         .setKeySignature("C Major")
-        .addNote(0, "C4", "4n", { string: 4, color: "blue" }).addAnnotation("staccato", { color: "green" })
+        .addNote(0, "C4", "4n", { string: 4, color: "blue" }).addAnnotation("staccato", { color: "green" }).addConnective("slur", 2, { color: "blue" })
         .addNote(0, "E4", "4n", { string: 3, color: "blue" }).addAnnotation("staccato", { color: "green" })
-        .addNote(0, "G4", "4n", { string: 2, color: "blue" }).addAnnotation("staccato", { color: "green" })
+        .addNote(0, "G4", "4n", { string: 2, color: "blue" }).addAnnotation("staccato", { color: "green" }).addConnective("slur", 2, { color: "blue" })
         .addNote(0, "C5", "4n", { string: 1, color: "blue" }).addAnnotation("staccato", { color: "green" })
         .addLyrics(1, "Lyrics...", "1n", { color: "green" })
         .getDocument();
