d2fa655b471e5a55a3da52a25749f1dd925588d8
Stefan <web-music-score@outlook.com>
2026-09-25 13:47:42 +0300

    Load wms dynamically for Showcase.

--- src/showcase/ViewSong.tsx
+++ src/showcase/ViewSong.tsx
@@ -1,16 +1,41 @@
 import BrowserOnly from "@docusaurus/BrowserOnly";
+import { useEffect, useState } from "react";
 
-import * as Core from "web-music-score-v7/core";
-import * as Audio from "web-music-score-v7/audio";
-import * as ReactUI from "web-music-score-v7/react-ui";
-import * as Score from "web-music-score-v7/score";
-import * as Pieces from "web-music-score-v7/pieces";
+import type { MDocument } from 'web-music-score-v7/score';
+
+async function loadWebMusicScore7() {
+    const [Core, Audio, ReactUI, Theory, Score, Pieces] = await Promise.all([
+        import('web-music-score-v7/core'),
+        import('web-music-score-v7/audio'),
+        import('web-music-score-v7/react-ui'),
+        import('web-music-score-v7/theory'),
+        import('web-music-score-v7/score'),
+        import('web-music-score-v7/pieces'),
+    ]);
+
+    return { Core, Audio, ReactUI, Theory, Score, Pieces };
+}
 
 export default function ViewSong(props: { songName: string }) {
-
     return <BrowserOnly>
         {() => {
-            let doc: Score.MDocument;
+
+            const [modules, setModules] = useState<Awaited<ReturnType<typeof loadWebMusicScore7>> | null>(null);
+
+            useEffect(() => {
+                loadWebMusicScore7().then(modules => {
+                    modules.Audio.setDefaultInstrument(0); // Play with "Acoustic Grand Piano"
+                    setModules(modules);
+                });
+            }, []);
+
+            if (!modules) {
+                return <div>Loading...</div>;
+            }
+
+            const { Core, ReactUI, Score, Pieces } = modules;
+
+            let doc: MDocument;
 
             switch (props.songName) {
                 case "AndanteByDiabelli":
@@ -29,8 +54,6 @@ export default function ViewSong(props: { songName: string }) {
                     doc = new Score.DocumentBuilder().getDocument();
                     break;
             }
-
-            Audio.setDefaultInstrument(0); // Play with "Acoustic Grand Piano"
 
             return <>
                 <ReactUI.WmsControls doc={doc} playPauseStop />
